42f493513c2552e2f8f5ce427f82d5aa4e164c6b
ogster <oliver.tate1@outlook.com>
2025-10-04 23:05:24 +0100

    Improved scoring

--- Columnar-Transposition-Solver/Program.cs
+++ Columnar-Transposition-Solver/Program.cs
@@ -9,16 +9,19 @@ namespace Columnar_Transposition_Solver
 {
     class Program
     {
+        private int l_;
+        private double[] scoresUsingInt_;
+
         public static void Main(string[] args)
         {
-
             Console.Write("Enter cipher text: ");
             string text = Console.ReadLine().ToLower();
-            List<string> texts = new();
+            double[] scoresUsingInt = InitiliseNgramsScorer();
+
 
             for (int keyLength = 2; 9 >= keyLength; keyLength++)
             {
-                object arg = new object[2] { text, keyLength };
+                object arg = new object[3] { text, keyLength, scoresUsingInt };
                 var T = new Thread(solve);
                 T.Start(arg);
             }
@@ -61,8 +64,8 @@ namespace Columnar_Transposition_Solver
             Array argArray = (Array)args;
             string text = (string)argArray.GetValue(0);
             int keyLength = (int)argArray.GetValue(1);
-            Ngrams ngrams = new Ngrams("english_quadgrams.txt");
-            List<Tuple<string, Double>> scores = new List<Tuple<string, double>>();
+            double[] scoresUsingInt = (double[])argArray.GetValue(2);
+            List<Tuple<IList<int>, Double>> scores = new List<Tuple<IList<int>, double>>();
             int length = Convert.ToInt32(Math.Ceiling(text.Length / Convert.ToDouble(keyLength)));
 
             int[] ArrayForPermutations = new int[keyLength];
@@ -72,18 +75,85 @@ namespace Columnar_Transposition_Solver
 
             foreach (IList<int> permutation in permutations)
             {
-                StringBuilder possibleString = new StringBuilder();
+                char[] buffer = new char[4];
+                bool bufferFull = false;
+                double score = 0;
+                int encoded = 0;
+                int mask = (1 << 15) - 1;
+
                 for (int firstD = 0; firstD < text.Length; firstD+=keyLength)
                 {
                     for (int secondD = 0; secondD < keyLength; secondD++)
                     {
                         if (firstD + permutation[secondD] >= text.Length){ break; }
-                        possibleString.Append(text[firstD+permutation[secondD]]);
+                        if (!bufferFull)
+                        {
+                            int index = firstD + permutation[secondD];
+                            char c = text[index];
+                            buffer[firstD + secondD] = text[firstD + permutation[secondD]];
+                            if (firstD + secondD >= 3)
+                            {
+                                bufferFull = true;
+                                encoded = EncodeLower(buffer);
+                                score += scoresUsingInt[encoded];
+                            }
+                        }
+                        else
+                        {
+                            encoded = ((encoded & mask) << 5) | (text[firstD + permutation[secondD]] - 'a');
+                            score += scoresUsingInt[encoded];
+                        }
                     }
                 }
-                scores.Add(new Tuple<string, Double>(possibleString.ToString(), ngrams.score(possibleString.ToString())));
+                scores.Add(new Tuple<IList<int>, Double>(permutation, score));
             }
-            Console.WriteLine(scores.MaxBy(t => t.Item2));
+            Console.WriteLine(scores.MaxBy(t => t.Item2) + " " + keyLength.ToString());
+        }
+
+        private static int EncodeLower(ReadOnlySpan<char> gram)
+        {
+            int value = 0;
+            foreach (char c in gram)
+            {
+                value = (value << 5) | (c - 'a');
+            }
+            return value;
+        }
+
+        private static int EncodeUpper(ReadOnlySpan<char> gram)
+        {
+            int value = 0;
+            foreach (char c in gram)
+            {
+                value = (value << 5) | (c - 'A');
+            }
+            return value;
+        }
+
+        private static double[] InitiliseNgramsScorer()
+        {
+            double sum_ = 0;
+            string[] lines = File.ReadAllLines("english_quadgrams.txt");
+
+            double[] scoresUsingInt = new double[(int)Math.Pow(32, 4)];
+            Array.Fill(scoresUsingInt, 0.01);
+
+            foreach (string item in lines)
+            {
+                string[] keyPair = item.Split(" ");
+                double value = double.Parse(keyPair[1]);
+                string key = keyPair[0];
+
+                scoresUsingInt[EncodeUpper(key)] = value;
+                sum_ += value;
+            }
+
+            for (int i = 0; i < scoresUsingInt.Length; i++)
+            {
+                scoresUsingInt[i] = Math.Log10(scoresUsingInt[i] / sum_);
+            }
+
+            return scoresUsingInt;
         }
     }
 }
--- Columnar-Transposition-Solver/obj/Debug/net6.0/Columnar-Transposition-Solver.AssemblyInfo.cs
+++ Columnar-Transposition-Solver/obj/Debug/net6.0/Columnar-Transposition-Solver.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Columnar-Transposition-Solver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd69359ae5c058b2194fd1b88f2f3688963cb23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Columnar-Transposition-Solver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Columnar-Transposition-Solver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
